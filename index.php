7a49d365a719239d644efabdd7f4cd95595f4dfa
Ralsywy <121875489+Ralsywy@users.noreply.github.com>
2023-05-10 10:33:20 +0200

    navbar

--- index.php
+++ index.php
@@ -10,6 +10,12 @@
     <script src='main.js'></script>
 </head>
 <body>
-    
+<nav class="navbar bg-body-tertiary">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Bootstrap" width="30" height="24">
+    </a>
+  </div>
+</nav>
 </body>
 </html>
