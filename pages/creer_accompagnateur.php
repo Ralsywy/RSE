6abff900e9df359f934b2fb4f4ae92084eba3cfd
Sommaly <vanh.sommaly@gmail.com>
2023-05-15 15:29:52 +0200

    page accompagnateur

--- pages/creer_accompagnateur.php
+++ pages/creer_accompagnateur.php
@@ -1,8 +1,25 @@
-<section class="creer_accompagnateur">
-<header>Créer un accompagnateur</header>
-<form action="#" class="form_accompagnateur"></form>
-<div class="input-box">
-    <label>full name</label>
-    <input type="text" placeholder="enter full name">
-</div>
+<div class="page_acc">
+<section class="creer_acc">
+    <h1 class="titre_acc">Créer un accompagnateur</h1>
+        <form action="#" class="form_acc"></form>
+        <div class="input-box_acc">
+            <label class="acc_name">NOM Prénom</label>
+            <input class="tbx_acc" type="text" placeholder="NOM Prénom de l'accompagnateur">
+            <a href="#" class="btn_acc_c">Créer</a>
+        </div>
 </section>
+<section class="liste_acc">
+    <h1 class="titre_acc">Liste des accompagnateurs</h1>
+    <!-- UN ACCOMPAGNATEUR -->
+    <div class="un_acc">
+    <div class="box_liste_acc">
+        <label class="liste_name_acc">NOM et Prénom</label>
+    </div>
+    <div class="btn_liste_acc">
+        <a href="#" class="btn_acc_m">Modifier</a>
+        <a href="#" class="btn_acc_s">Supprimer</a>
+    </div>
+    </div>
+    <!-- FIN D'UN ACCOMPAGNATEUR -->
+</section>
+</div>
