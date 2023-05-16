190d5a88108041cc5ce5e141b39ab5b72eab1be1
Sommaly <vanh.sommaly@gmail.com>
2023-05-16 10:10:38 +0200

    centre acc

--- index.php
+++ index.php
@@ -19,9 +19,10 @@ session_start();
 
 <!-- DEBUT NAVBAR -->
 <header>
-<nav class="navbar navbar-expand-lg" style="background-color: #ff7dc8;">
+<nav class="navbar navbar-expand-lg">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">
+      
       <img src="img/rdc.png" width="50" height="50">
     </a>
     <div class="collapse navbar-collapse" id="navbarNav">
--- pages/creer_accompagnateur.php
+++ pages/creer_accompagnateur.php
@@ -1,25 +1,75 @@
 <div class="page_acc">
-<section class="creer_acc">
-    <h1 class="titre_acc">Créer un accompagnateur</h1>
-        <form action="#" class="form_acc"></form>
-        <div class="input-box_acc">
-            <label class="acc_name">NOM Prénom</label>
-            <input class="tbx_acc" type="text" placeholder="NOM Prénom de l'accompagnateur">
-            <a href="#" class="btn_acc_c">Créer</a>
+    <div class="creer_acc">
+        <h1 class="titre_acc">Créer un accompagnateur</h1>
+            <form action="#" class="form_acc"></form>
+            <div class="input-box_acc">
+                <label class="acc_name">NOM Prénom</label>
+                <input class="tbx_acc" type="text" placeholder="NOM Prénom de l'accompagnateur">
+                <a href="#" class="btn_acc_c">Créer</a>
+            </div>
+    </div>
+    <section class="liste_acc">
+        <h1 class="titre_acc">Liste des accompagnateurs</h1>
+        <!-- UN ACCOMPAGNATEUR -->
+        <div class="un_acc">
+        <div class="box_liste_acc">
+            <label class="liste_name_acc">NOM et Prénom</label>
         </div>
-</section>
-<section class="liste_acc">
-    <h1 class="titre_acc">Liste des accompagnateurs</h1>
-    <!-- UN ACCOMPAGNATEUR -->
-    <div class="un_acc">
-    <div class="box_liste_acc">
-        <label class="liste_name_acc">NOM et Prénom</label>
-    </div>
-    <div class="btn_liste_acc">
-        <a href="#" class="btn_acc_m">Modifier</a>
-        <a href="#" class="btn_acc_s">Supprimer</a>
-    </div>
-    </div>
-    <!-- FIN D'UN ACCOMPAGNATEUR -->
-</section>
+        <div class="btn_liste_acc">
+            <a href="#" class="btn_acc_m">Modifier</a>
+            <a href="#" class="btn_acc_s">Supprimer</a>
+        </div>
+        </div>
+        <!-- FIN D'UN ACCOMPAGNATEUR -->
+        <div class="un_acc">
+        <div class="box_liste_acc">
+            <label class="liste_name_acc">NOM et Prénom</label>
+        </div>
+        <div class="btn_liste_acc">
+            <a href="#" class="btn_acc_m">Modifier</a>
+            <a href="#" class="btn_acc_s">Supprimer</a>
+        </div>
+        </div><div class="un_acc">
+        <div class="box_liste_acc">
+            <label class="liste_name_acc">NOM et Prénom</label>
+        </div>
+        <div class="btn_liste_acc">
+            <a href="#" class="btn_acc_m">Modifier</a>
+            <a href="#" class="btn_acc_s">Supprimer</a>
+        </div>
+        </div><div class="un_acc">
+        <div class="box_liste_acc">
+            <label class="liste_name_acc">NOM et Prénom</label>
+        </div>
+        <div class="btn_liste_acc">
+            <a href="#" class="btn_acc_m">Modifier</a>
+            <a href="#" class="btn_acc_s">Supprimer</a>
+        </div>
+        </div>
+        <div class="un_acc">
+        <div class="box_liste_acc">
+            <label class="liste_name_acc">NOM et Prénom</label>
+        </div>
+        <div class="btn_liste_acc">
+            <a href="#" class="btn_acc_m">Modifier</a>
+            <a href="#" class="btn_acc_s">Supprimer</a>
+        </div>
+        </div><div class="un_acc">
+        <div class="box_liste_acc">
+            <label class="liste_name_acc">NOM et Prénom</label>
+        </div>
+        <div class="btn_liste_acc">
+            <a href="#" class="btn_acc_m">Modifier</a>
+            <a href="#" class="btn_acc_s">Supprimer</a>
+        </div>
+        </div><div class="un_acc">
+        <div class="box_liste_acc">
+            <label class="liste_name_acc">NOM et Prénom</label>
+        </div>
+        <div class="btn_liste_acc">
+            <a href="#" class="btn_acc_m">Modifier</a>
+            <a href="#" class="btn_acc_s">Supprimer</a>
+        </div>
+        </div>
+    </section>
 </div>
