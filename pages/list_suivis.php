bfcf9b2f0d7d67921fe00922031c41158775584a
Sommaly <vanh.sommaly@gmail.com>
2023-05-24 11:16:27 +0200

    test

--- pages/list_suivis.php
+++ pages/list_suivis.php
@@ -1,3 +1,7 @@
+<form method="GET">
+
+</form>
+
 <div class="grille">
     <div class="grid-item">
         <p class="nom">Nom</p>
@@ -6,48 +10,93 @@
         <a href="#" class="btn1">Modifier</a>
     </div>
     <div class="grid-item">
-    <p class="nom">Nom</p>
-    <p class="prenom">Prénom</p>
+        <p class="nom">Nom</p>
+        <p class="prenom">Prénom</p>
+        <p class="sexe">Sexe</p>
         <a href="#" class="btn1">Modifier</a>
     </div>
     <div class="grid-item">
-    <p class="nom">Nom</p>
-    <p class="prenom">Prénom</p>
+        <p class="nom">Nom</p>
+        <p class="prenom">Prénom</p>
+        <p class="sexe">Sexe</p>
         <a href="#" class="btn1">Modifier</a>
     </div>
     <div class="grid-item">
-    <p class="nom">Nom</p>
-    <p class="prenom">Prénom</p>
+        <p class="nom">Nom</p>
+        <p class="prenom">Prénom</p>
+        <p class="sexe">Sexe</p>
         <a href="#" class="btn1">Modifier</a>
     </div>
     <div class="grid-item">
-    <p class="nom">Nom</p>
-    <p class="prenom">Prénom</p>
+        <p class="nom">Nom</p>
+        <p class="prenom">Prénom</p>
+        <p class="sexe">Sexe</p>
         <a href="#" class="btn1">Modifier</a>
     </div>
     <div class="grid-item">
-    <p class="nom">Nom</p>
-    <p class="prenom">Prénom</p>
+        <p class="nom">Nom</p>
+        <p class="prenom">Prénom</p>
+        <p class="sexe">Sexe</p>
         <a href="#" class="btn1">Modifier</a>
     </div>
     <div class="grid-item">
-    <p class="nom">Nom</p>
-    <p class="prenom">Prénom</p>
+        <p class="nom">Nom</p>
+        <p class="prenom">Prénom</p>
+        <p class="sexe">Sexe</p>
         <a href="#" class="btn1">Modifier</a>
     </div>
     <div class="grid-item">
-    <p class="nom">Nom</p>
-    <p class="prenom">Prénom</p>
+        <p class="nom">Nom</p>
+        <p class="prenom">Prénom</p>
+        <p class="sexe">Sexe</p>
         <a href="#" class="btn1">Modifier</a>
     </div>
     <div class="grid-item">
-    <p class="nom">Nom</p>
-    <p class="prenom">Prénom</p>
+        <p class="nom">Nom</p>
+        <p class="prenom">Prénom</p>
+        <p class="sexe">Sexe</p>
         <a href="#" class="btn1">Modifier</a>
     </div>
     <div class="grid-item">
-    <p class="nom">Nom</p>
-    <p class="prenom">Prénom</p>
+        <p class="nom">Nom</p>
+        <p class="prenom">Prénom</p>
+        <p class="sexe">Sexe</p>
+        <a href="#" class="btn1">Modifier</a>
+    </div>
+    <div class="grid-item">
+        <p class="nom">Nom</p>
+        <p class="prenom">Prénom</p>
+        <p class="sexe">Sexe</p>
+        <a href="#" class="btn1">Modifier</a>
+    </div>
+    <div class="grid-item">
+        <p class="nom">Nom</p>
+        <p class="prenom">Prénom</p>
+        <p class="sexe">Sexe</p>
+        <a href="#" class="btn1">Modifier</a>
+    </div>
+    <div class="grid-item">
+        <p class="nom">Nom</p>
+        <p class="prenom">Prénom</p>
+        <p class="sexe">Sexe</p>
+        <a href="#" class="btn1">Modifier</a>
+    </div>
+    <div class="grid-item">
+        <p class="nom">Nom</p>
+        <p class="prenom">Prénom</p>
+        <p class="sexe">Sexe</p>
+        <a href="#" class="btn1">Modifier</a>
+    </div>
+    <div class="grid-item">
+        <p class="nom">Nom</p>
+        <p class="prenom">Prénom</p>
+        <p class="sexe">Sexe</p>
+        <a href="#" class="btn1">Modifier</a>
+    </div>
+    <div class="grid-item">
+        <p class="nom">Nom</p>
+        <p class="prenom">Prénom</p>
+        <p class="sexe">Sexe</p>
         <a href="#" class="btn1">Modifier</a>
     </div>
     
