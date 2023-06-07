b26ee313261a18e31903ed4003d1451d48fc0ed3
Ralsywy <121875489+Ralsywy@users.noreply.github.com>
2023-06-07 12:59:28 +0200

    ajouts suivis

--- pages/creer_suivis/store_plan.php
+++ pages/creer_suivis/store_plan.php
@@ -1,6 +1,6 @@
 <?php
 
-if (isset($_POST["action_menee"])==false || empty($_POST["action_menee"]) || isset($_POST["objectif"])==false || empty($_POST["objectif"]) || isset($_POST["moyen_oeuvre"])==false || empty($_POST["moyen_oeuvre"] || isset($_POST["echeance"])==false || empty($_POST["echeance"]))){
+if (isset($_POST["action_menee"])==false || empty($_POST["action_menee"]) || isset($_POST["echeance"])==false || empty($_POST["echeance"])){
     $_SESSION["error"]="Tout les champs sont obligatoires";
     header("location:index.php?route=creer2");
 }
@@ -15,9 +15,9 @@ else
     );
     
     // ordre de mission
-    $requete = $mysqlConnection->prepare('INSERT INTO plan_action(action_menee,objectif,moyen_oeuvre,echeance) values(:action_menee,:objectif,:moyen_oeuvre,:echeance)');
+    $requete = $mysqlConnection->prepare('INSERT INTO plan_action(action_menee,echeance) values(:action_menee,:echeance)');
     //execution de la requete
-    $requete->execute( ["action_menee"=>$_POST["action_menee"],"objectif"=>$_POST["objectif"],"moyen_oeuvre"=>$_POST["moyen_oeuvre"],"echeance"=>$_POST["echeance"]]);
+    $requete->execute( ["action_menee"=>$_POST["action_menee"],"echeance"=>$_POST["echeance"]]);
     $mysqlConnection = null;
     $requete = null;
 
